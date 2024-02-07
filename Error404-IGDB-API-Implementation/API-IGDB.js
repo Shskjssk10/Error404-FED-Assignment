b0faded72111e250210ba45498b16faa5562a291
Shskjssk10 <133301405+Shskjssk10@users.noreply.github.com>
2024-02-07 15:22:43 +0800

    Styling is done, left with responsiveness and positioning

--- Error404-IGDB-API-Implementation/API-IGDB.js
+++ Error404-IGDB-API-Implementation/API-IGDB.js
@@ -3,7 +3,7 @@ const clientID = "r3auekm7tcozzfu1pi7xpv2cl7kqzt";
 const clientSecret = "6w7djtd2t5fqom1fy20yui0yh1ae4l";
 let accessToken;
 const authUrl = `https://id.twitch.tv/oauth2/token`;
-const serverUrl = "http://localhost:8081/getGames";
+const serverUrl = "http://localhost:1115/getGames";
 
 fetch(authUrl, {
   method: "POST",
@@ -36,30 +36,26 @@ fetch(authUrl, {
       .then((response) => {
         console.log(response);
         //clear previous content
-        cardContainer.innerHTML = "";
+        const overallContainer = document.querySelector(".container");
 
         //loop through and create each card to display for each game
         for (var i = 0; i < response.length; i++) {
           const game = response[i];
 
-          //create bs card dynamically
+          //create card container dynamically
           //create a new div element
-          const cardDiv = document.createElement("div");
-          cardDiv.classList.add("col-md-4", "mb-4", "card-wrapper");
-
-          //create card
-          const cardBodyDiv = document.createElement("div");
-          cardBodyDiv.classList.add("card", "h-100", "card-body-wrapper");
+          const cardContainer = document.createElement("div");
+          cardContainer.classList.add("col-lg-4", "col-md-4", "col-sm-4","mb-4", "card-container")
 
           //title from igdb api
-          const title = document.createElement("h5");
-          title.classList.add("card-title");
-          title.textContent = game.name;
-          cardBodyDiv.appendChild(title);
+          const titleDiv = document.createElement("div");
+          titleDiv.classList.add("second-tier-header", "game-title");
+          titleDiv.textContent = game.name;
+          cardContainer.appendChild(titleDiv);
 
-          //content
-          const contentDiv = document.createElement("div");
-          contentDiv.classList.add("card-text");
+          //content, will store summary
+          const sumDiv = document.createElement("div");
+          sumDiv.classList.add("text", "game-summary");
 
           //set summary else if game.summary is empty then show default text
           const summaryText = game.summary
@@ -71,39 +67,39 @@ fetch(authUrl, {
             summaryText.length > 150
               ? summaryText.substring(0, 150) + "..."
               : summaryText;
-          contentDiv.textContent = Summary;
-          cardBodyDiv.appendChild(contentDiv);
+              sumDiv.textContent = Summary;
+          cardContainer.appendChild(sumDiv);
 
           //if more than 150 then let user decide is they want to click show more
           if (summaryText.length > 150) {
             const readMoreButton = document.createElement("button");
-            readMoreButton.classList.add("btn", "btn-link");
+            readMoreButton.classList.add("text");
             readMoreButton.textContent = "Show More";
             readMoreButton.addEventListener("click", () => {
-              if (contentDiv.textContent === Summary) {
-                contentDiv.textContent = summaryText;
+              if (sumDiv.textContent === Summary) {
+                sumDiv.textContent = summaryText;
                 readMoreButton.textContent = "Show Less";
               } else {
-                contentDiv.textContent = Summary;
+                sumDiv.textContent = Summary;
                 readMoreButton.textContent = "Show More";
               }
             });
-            cardBodyDiv.appendChild(readMoreButton);
+            cardContainer.appendChild(readMoreButton);
           }
 
           //button links to igdb website fot the specific game
-          const button = document.createElement("a");
-          button.href = game.url;
-          button.classList.add("btn", "btn-primary", "mt-auto");
-          button.textContent = "Find Out More";
-          cardBodyDiv.appendChild(button);
+          const buttonContainer = document.createElement("a");
+          buttonContainer.href = game.url;
+          const buttonText = document.createElement("div");
+          buttonText.textContent = "Find Out More"
+          buttonText.classList.add("button", "button-text", "find-out-more-button");
+
+          buttonContainer.appendChild(buttonText);
+          cardContainer.appendChild(buttonContainer);
 
           //append to card
-          cardDiv.appendChild(cardBodyDiv);
-          cardContainer.appendChild(cardDiv);
+          overallContainer.appendChild(cardContainer);
         }
-
-        cardContainer.classList.add("justify-content-around", "d-flex");
       })
       .catch((err) => {
         console.error(err);
