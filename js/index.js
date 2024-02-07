9aad42bacb86d8ff8809e66b9bbbb4aaeae3e622
Shskjssk10 <cadentohjunyi@gmail.com>
2024-02-08 07:45:50 +0800

    Filter Implementation. Filter works however styling is buggy

---
+++ js/index.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Event listener for search button
+    document.querySelector('.search-button').addEventListener('click', filterGames);
+  
+    function filterGames() {
+      var category = document.querySelector('.dropdown-filter').value.toLowerCase();
+      var search = document.querySelector('#search').value.toLowerCase();
+  
+      var rows = document.querySelectorAll('.row');
+      rows.forEach(function (row) {
+        var rowCategory = row.dataset.category.toLowerCase();
+        var rowName = row.querySelector('.second-tier-header').textContent.toLowerCase();
+  
+        var categoryMatch = (category === 'both' || rowCategory === category);
+        var searchMatch = (rowName.includes(search));
+  
+        if (categoryMatch && searchMatch) {
+          row.style.display = 'block';
+        } else {
+          row.style.display = 'none';
+        }
+      });
+    }
+  });
+  
