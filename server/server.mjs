02a94033285ab89f20f21ed8c4993d8ad5f21c73
Hendrik Yong <hendrikyong1205@gmail.com>
2024-02-08 18:22:53 +0800

    updated server mjs link

--- server/server.mjs
+++ server/server.mjs
@@ -8,10 +8,7 @@ const clientID = "r3auekm7tcozzfu1pi7xpv2cl7kqzt";
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://shskjssk10.github.io/Error404-FED-Assignment/"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "https://shskjssk10.github.io");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
